d7b6d3eb74250989b2ad9cdf0e7e0075826cd74f
Evgeniy Dolgopolov <78054338+JohnGlod@users.noreply.github.com>
2023-03-16 09:59:49 +0500

    [FIX] Fix adaptive for ProjectItem (#12)
    * feat: update projects & about
    
    * feat: update ProjectItem
    
    * feat: add files for deploy
    
    * fix: change deploy target
    
    * fix: fix adaptive  ProjectItem

--- components/ProjectItem.vue
+++ components/ProjectItem.vue
@@ -18,27 +18,11 @@ export default {
 <template>
   <article class="project">
     <div class="container">
-      <div
-        class="project__inner"
-        :data-aos="animate.name"
-        data-aos-duration="1000"
-        data-aos-anchor-placement="top-bottom"
-        :data-aos-delay="animate.delay"
-        data-aos-once="true"
-      >
-        <a
-          :href="project.demo"
-          data-aos="zoom-in-up"
-          data-aos-duration="1000"
-          data-aos-anchor-placement="top-bottom"
-          :data-aos-delay="animate.delay + 200"
-          data-aos-once="true"
-        >
-          <img
-            class="project__image"
-            :src="project.image.src"
-            :alt="project.title"
-          />
+      <div class="project__inner" :data-aos="animate.name" data-aos-duration="1000" data-aos-anchor-placement="top-bottom"
+        :data-aos-delay="animate.delay" data-aos-once="true">
+        <a :href="project.demo" data-aos="zoom-in-up" data-aos-duration="1000" data-aos-anchor-placement="top-bottom"
+          :data-aos-delay="animate.delay + 200" data-aos-once="true">
+          <img class="project__image" :src="project.image.src" :alt="project.title" />
         </a>
         <div>
           <h3
@@ -52,48 +36,24 @@ export default {
             {{ project.title }}
           </h3>
 
-          <h4
-            class="subtitle"
-            data-aos="fade-left"
-            data-aos-duration="800"
-            data-aos-anchor-placement="top-bottom"
-            :data-aos-delay="animate.delay + 600"
-            data-aos-once="true"
-          >
+          <h4 class="subtitle" data-aos="fade-left" data-aos-duration="800" data-aos-anchor-placement="top-bottom"
+            :data-aos-delay="animate.delay + 600" data-aos-once="true">
             Цель проекта:
           </h4>
 
           <ul class="project__list">
-            <li
-              v-for="(item, id) in project.list"
-              :key="id"
-              data-aos="fade-left"
-              data-aos-duration="800"
-              data-aos-anchor-placement="top-bottom"
-              :data-aos-delay="animate.delay + 600 + id * 100"
-              data-aos-once="true"
-            >
+            <li v-for="(item, id) in project.list" :key="id" data-aos="fade-left" data-aos-duration="800"
+              data-aos-anchor-placement="top-bottom" :data-aos-delay="animate.delay + 600 + id * 100"
+              data-aos-once="true">
               {{ item }}
             </li>
           </ul>
-          <div
-            data-aos="fade-left"
-            data-aos-duration="800"
-            data-aos-anchor-placement="top-bottom"
-            data-aos-delay="2000"
-            data-aos-once="true"
-            class="links"
-          >
+          <div data-aos="fade-left" data-aos-duration="800" data-aos-anchor-placement="top-bottom" data-aos-delay="2000"
+            data-aos-once="true" class="links">
             <a class="links__item" :href="project.demo">Демо </a>
             |
-            <a
-              class="links__item"
-              :href="project.repo"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Код</a
-            >
+            <a class="links__item" :href="project.repo" target="_blank" rel="noopener noreferrer">
+              Код</a>
           </div>
         </div>
       </div>
@@ -105,8 +65,16 @@ export default {
 .project {
   overflow: hidden;
   padding-top: 50px;
+
   &__inner {
-    columns: 2 var(--content);
+    @media screen and (min-width: 1024px) {
+      flex-direction: row;
+    }
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
     margin-bottom: var(--margin-big);
   }
 
@@ -119,12 +87,14 @@ export default {
     height: 90%;
     margin-bottom: var(--margin-big);
     transition: transform ease-in 0.8s;
+
     @media screen and (min-width: 767px) {
       object-fit: cover;
       border: 4px solid #fff;
       box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1),
         0 4px 6px -4px rgb(0 0 0 / 0.1);
     }
+    
     &:hover {
       transform: scale(1.05);
     }
@@ -133,7 +103,8 @@ export default {
   &__list {
     margin-bottom: var(--margin-lg);
     padding-left: var(--margin-lg);
-    > li {
+
+    >li {
       margin-bottom: var(--margin-small);
       padding-left: 10px;
       list-style: '✅';
